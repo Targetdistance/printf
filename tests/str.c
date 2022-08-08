dbfb39115b50334b976c5582ff935fc97824cdf2
Aghogho Ken-Erhimu <aghoghoken36@gmail.com>
2022-08-08 05:58:30 -0700

    testing return value

--- tests/str.c
+++ tests/str.c
@@ -2,12 +2,16 @@
 
 int main(void)
 {
-	int n, a;
+	int n, a, len1, len2;
 
+	len1 = _printf("My name is %s, and my math grade is %c\n", "Joseph", 'D');
+	len2 = printf("My name is %s, and my math grade is %c\n", "Joseph", 'D');
 	n = _printf("trash%%\n");
 	printf("n = %d\n", n);
 	a = printf("trash%%\n");
 	printf("a = %d\n", a);
+	printf("length: %d\n", len1);
+	printf("length: %d\n", len2);
 
 	return (0);
 }
